4b2d5cdf7d3668e875e080f6677a4b0e97b0d12d
sonic1573 <sonic157346@gmail.com>
2020-02-18 23:55:06 +0900

    유도탄

---
+++ Assets/01.Scripts/Create_Bullet.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Create_Bullet : MonoBehaviour
+{
+    public LayerMask LayerEnemy;
+    public GameObject Bullet;
+    Create_Bullet bullet;
+
+    public float tileDamage = 5f;
+    private float rangeRadius = 2f;
+
+    public float TimeLeft = 1.0f;
+    private float nextTime = 0.0f;
+
+    //Vector2 vector_Test;
+
+    void Start()
+    {
+        StartCoroutine(Wait());
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(3f);
+    }
+
+    void Update()
+    {
+        if (Time.time > nextTime)
+        {
+            nextTime = Time.time + TimeLeft;
+            exploration();
+        }
+    }
+
+
+    private void exploration()
+    {
+        //Bullet.bulletDamage = tileDamage;
+
+        Instantiate(Bullet, this.transform.position, this.transform.rotation);
+    }
+
+
+
+
+}
---
+++ Assets/01.Scripts/Fire.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class Fire : MonoBehaviour
+{
+    public List<GameObject> FoundObjects;
+    public GameObject enemy;
+    public string TagName;
+    public float shortDis;
+
+    public float speed = 50f;
+    private float startTime;
+    private float distanceLength;
+
+    private Vector3 StartPosition;
+    private Vector3 EndPosition;
+    private Vector3 currPosition;
+
+    public void Update()
+    {
+        FoundObjects = new List<GameObject>(GameObject.FindGameObjectsWithTag(TagName));
+        shortDis = Vector3.Distance(gameObject.transform.position, FoundObjects[0].transform.position); // 첫번째를 기준으로 잡아주기 
+
+        enemy = FoundObjects[0]; // 첫번째를 먼저 
+
+        foreach (GameObject found in FoundObjects)
+        {
+            float Distance = Vector3.Distance(gameObject.transform.position, found.transform.position);
+
+            if (Distance < shortDis) // 위에서 잡은 기준으로 거리 재기
+            {
+                enemy = found;
+                shortDis = Distance;
+            }
+
+        }
+        //Debug.Log(enemy.transform.position);
+
+        StartPosition = transform.position;
+        EndPosition = new Vector3(enemy.transform.position.x, enemy.transform.position.y, 0);
+
+        startTime = Time.time;
+        distanceLength = Vector3.Distance(StartPosition, EndPosition);
+
+
+        transform.Translate(Vector3.up * Time.deltaTime);
+
+        currPosition = transform.position;
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(currPosition, EndPosition, step);
+    }
+
+
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
